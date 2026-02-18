59270d75665aa709c583c4861fbddf34b97366ec
Rachel Tirkey <racheltirkey.in@gmail.com>
2026-02-18 14:36:56 +0530

    class-1: added a person firstname and age

--- class-1/main.js
+++ class-1/main.js
@@ -1,1 +1,6 @@
-console.log("hello world");
+// console.log("hello world");
+
+let firstName  = "rachel";
+let age = 25;
+let isMarried = false;
+ console.log("this person name is " + firstName + " and their age is " + age);
